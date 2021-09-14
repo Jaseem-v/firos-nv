1ece99f6e41f28eb8626b8930703f24ba2ed2429
Jaseem-v <muhammedjaseem753@gmail.com>
2021-09-14 08:11:47 +0100

    photo hover issue fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*********************************************************************************
 
-	Template Name: Keystroke Creative Agency Bootstrap4 Html5 Template
-	Note: This is main js.
+    Template Name: Keystroke Creative Agency Bootstrap4 Html5 Template
+    Note: This is main js.
 
 **********************************************************************************/
 (function (window, document, $, undefined) {
@@ -9,16 +9,16 @@
 
     var axilKey = {
         k: function (e) {
-			axilKey.s();
-			axilKey.methods();
+            axilKey.s();
+            axilKey.methods();
         },
         s: function (e) {
-			this._window = $(window),
-			this._document = $(document),
-			this._body = $('body'),
-            this._html = $('html'),
-            this.sideNav = $('.side-nav'),
-            this._navsearch = $('.axil-search-area')
+            this._window = $(window),
+                this._document = $(document),
+                this._body = $('body'),
+                this._html = $('html'),
+                this.sideNav = $('.side-nav'),
+                this._navsearch = $('.axil-search-area')
         },
         methods: function (e) {
             axilKey.axilWow();
@@ -55,17 +55,17 @@
                 $(this).toggleClass('active').siblings('.submenu').toggleClass('is-visiable')
             })
         },
-        
+
         stickHeader: function () {
             axilKey._window.scroll(function () {
                 if ($(this).scrollTop() > 250) {
                     $('.axil-header-sticky').addClass('sticky')
-                }else{
+                } else {
                     $('.axil-header-sticky').removeClass('sticky')
                 }
             })
         },
-        
+
         mouseMOve: function () {
             $.fn.parallax = function (resistance, mouse) {
                 var $el;
@@ -119,14 +119,14 @@
 
 
         counterUp: function () {
-			var _counter = $('.count');
-			if (_counter.length) {
-				_counter.counterUp({
-					delay: 10,
-					time: 1000,
-					triggerOnce: true
-				});
-			}
+            var _counter = $('.count');
+            if (_counter.length) {
+                _counter.counterUp({
+                    delay: 10,
+                    time: 1000,
+                    triggerOnce: true
+                });
+            }
         },
 
         ytPlayer: function () {
@@ -162,20 +162,20 @@
                         filter: filterValue
                     });
                 });
-                
+
                 // init Isotope
-                var $grid = $('.mesonry-list').isotope({
-                    itemSelector: '.portfolio',
-                    percentPosition: true,
-                    transitionDuration: '0.7s',
-                    layoutMode: 'fitRows',
-                    masonry: {
-                        // use outer width of grid-sizer for columnWidth
-                        columnWidth: 1,
-                    }
-                });
+                // var $grid = $('.mesonry-list').isotope({
+                //     itemSelector: '.portfolio',
+                //     percentPosition: true,
+                //     transitionDuration: '0.7s',
+                //     layoutMode: 'fitRows',
+                //     masonry: {
+                //         // use outer width of grid-sizer for columnWidth
+                //         columnWidth: 1,
+                //     }
+                // });
             });
-        
+
             $('.messonry-button button').on('click', function (event) {
                 $(this).siblings('.is-checked').removeClass('is-checked');
                 $(this).addClass('is-checked');
@@ -216,21 +216,21 @@
             })
         },
 
-        axilHover : function () {
-            $('.axil-service-area .axil-service , .mesonry-list .portfolio , .blog-list-wrapper .axil-blog, .axil-testimonial-single .axil-testimonial').mouseenter(function() {
-                var self=this;
+        axilHover: function () {
+            $('.axil-service-area .axil-service , .mesonry-list .portfolio , .blog-list-wrapper .axil-blog, .axil-testimonial-single .axil-testimonial').mouseenter(function () {
+                var self = this;
                 $(self).removeClass('axil-control');
-                setTimeout(function() {
+                setTimeout(function () {
                     $('.axil-service-area .active , .mesonry-list .active , .blog-list-wrapper .active,  .axil-testimonial-single .active').removeClass('active').addClass('axil-control');
                     $(self).removeClass('axil-control').addClass('active');
-                    $('.axil-service.active .inner::before').css('opacity',0.1);
+                    $('.axil-service.active .inner::before').css('opacity', 0.1);
                 }, 0);
-                
+
             });
         },
 
         scrollDown: function () {
-            $('.smoth-animation').click(function() {
+            $('.smoth-animation').click(function () {
                 $('html, body').animate({
                     // scrollTop: $("#sectionBottom").offset().top
                 }, 1000);
@@ -240,7 +240,7 @@
         axilaccor: function () {
             $('.axil-accordion .card .card-header').siblings('.collapse.show').parent().addClass('open');
         },
-        
+
         _slickDoc: function (e) {
             // Check if element exists
             $.fn.elExists = function () {
@@ -249,7 +249,7 @@
             // Variables
             var $html = $('html'),
                 $elementCarousel = $('.axil-carousel');
-    
+
             if ($elementCarousel.elExists()) {
                 var slickInstances = [];
                 $elementCarousel.each(function (index, element) {
@@ -269,7 +269,7 @@
                         $autoplaySpeed = $options.autoplaySpeed ? $options.autoplaySpeed : 5000,
                         $swipe = $options.swipe ? $options.swipe : false,
                         $adaptiveHeight = $options.adaptiveHeight ? $options.adaptiveHeight : false,
-    
+
                         $arrows = $options.arrows ? $options.arrows : false,
                         $dots = $options.dots ? $options.dots : false,
                         $infinite = $options.infinite ? $options.infinite : false,
@@ -280,21 +280,21 @@
                         $nextArrow = $arrows === true ? ($options.nextArrow ? '<span class="' + $options.nextArrow.buttonClass + '"><i class="' + $options.nextArrow.iconClass + '"></i></span>' : '<button class="slick-next">next</span>') : '',
                         $slidesToShow = $options.slidesToShow ? parseInt($options.slidesToShow, 10) : 1,
                         $slidesToScroll = $options.slidesToScroll ? parseInt($options.slidesToScroll, 10) : 1;
-    
+
                     /*Responsive Variable, Array & Loops*/
                     var $responsiveSetting = typeof $this.data('slick-responsive') !== 'undefined' ? $this.data('slick-responsive') : '',
                         $responsiveSettingLength = $responsiveSetting.length,
                         $responsiveArray = [];
                     for (var i = 0; i < $responsiveSettingLength; i++) {
                         $responsiveArray[i] = $responsiveSetting[i];
-    
+
                     }
-    
+
                     // Adding Class to instances
                     $this.addClass('slick-carousel-' + index);
                     $this.parent().find('.slick-dots').addClass('dots-' + index);
                     $this.parent().find('.slick-btn').addClass('btn-' + index);
-    
+
                     if ($spaceBetween != 0) {
                         $this.addClass('slick-gutter-' + $spaceBetween);
                     }
@@ -321,7 +321,7 @@
                         nextArrow: $nextArrow,
                         responsive: $responsiveArray,
                     });
-    
+
                     if ($isCustomArrow === true) {
                         $($customPrev).on('click', function () {
                             $this.slick('slickPrev');
@@ -331,25 +331,25 @@
                         });
                     }
                 });
-    
+
                 // Updating the sliders in tab
                 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
                     $elementCarousel.slick('setPosition');
                 });
             }
         },
-        
+
         _clickDoc: function (e) {
-            var inputblur, inputFocus, openSideNav, closeSideNav, openSubMenu , closeSubMenu, searchTriggerShow , searchTriggerHide, axilaccordion, OpenMobileMenu, closeMobileMenu, closeMenuWrapperClick, closeMobileMenu2;
-            
+            var inputblur, inputFocus, openSideNav, closeSideNav, openSubMenu, closeSubMenu, searchTriggerShow, searchTriggerHide, axilaccordion, OpenMobileMenu, closeMobileMenu, closeMenuWrapperClick, closeMobileMenu2;
+
             inputblur = function (e) {
-				if (!$(this).val()) {
-					$(this).parent('.form-group').removeClass('focused');
-				}
+                if (!$(this).val()) {
+                    $(this).parent('.form-group').removeClass('focused');
+                }
             };
 
             inputFocus = function (e) {
-				$(this).parents('.form-group').addClass('focused');
+                $(this).parents('.form-group').addClass('focused');
             };
 
             openSideNav = function (e) {
@@ -359,8 +359,8 @@
             };
 
             closeSideNav = function (e) {
-				if (!$('.side-nav, .side-nav *:not(".close-sidenav, .close-sidenav *")').is(e.target)) {
-					axilKey.sideNav.removeClass('opened');
+                if (!$('.side-nav, .side-nav *:not(".close-sidenav, .close-sidenav *")').is(e.target)) {
+                    axilKey.sideNav.removeClass('opened');
                     axilKey._html.removeClass('side-nav-opened');
                 }
             };
@@ -375,9 +375,9 @@
 
             closeMenuWrapperClick = function (e) {
                 e.target === this && axilKey._body.removeClass('popup-mobile-manu-visible'),
-                axilKey._html.css({
-                    overflow: ''
-                });
+                    axilKey._html.css({
+                        overflow: ''
+                    });
             };
 
             closeMobileMenu = function (e) {
@@ -391,7 +391,7 @@
             openSubMenu = function (e) {
                 if (axilKey._window.width() < 992) {
                     $(this).siblings('.axil-submenu').slideToggle('active').parents('li').toggleClass('active')
-                }else {
+                } else {
                     $(this).siblings('.axil-submenu').toggleClass('visible').parents('li').toggleClass('active');
                     $(this).parents('li').siblings('.has-dropdown').removeClass('active').find('.axil-submenu').removeClass('visible')
                 }
@@ -412,7 +412,7 @@
                 e.preventDefault();
                 axilKey._navsearch.removeClass('visible')
             };
-            
+
             axilaccordion = function (e) {
                 e.preventDefault();
                 $(this).siblings('.collapse.show').parent().removeClass('open').toggleClass('active');
@@ -425,12 +425,12 @@
                 .on('click', '#close-sidenav , .side-nav-opened', closeSideNav)
                 // .on('click', '.mainmenu-nav ul.mainmenu li a', openSubMenu )
                 // .on('click' , closeSubMenu)
-                .on('click' , '.search-trigger' , searchTriggerShow)
-                .on('click' , '.axil-search-area .navbar-search-close' , searchTriggerHide)
-                .on('click' , '.axil-accordion .card .card-header' , axilaccordion)
-                .on('click' , '.popup-navigation-activation' , OpenMobileMenu)
-                .on('click' , '.close-menu' , closeMobileMenu)
-                .on('click' , '.popup-mobile-manu' , closeMenuWrapperClick)
+                .on('click', '.search-trigger', searchTriggerShow)
+                .on('click', '.axil-search-area .navbar-search-close', searchTriggerHide)
+                .on('click', '.axil-accordion .card .card-header', axilaccordion)
+                .on('click', '.popup-navigation-activation', OpenMobileMenu)
+                .on('click', '.close-menu', closeMobileMenu)
+                .on('click', '.popup-mobile-manu', closeMenuWrapperClick)
         }
     };
 
