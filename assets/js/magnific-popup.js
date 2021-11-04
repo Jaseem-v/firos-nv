0f2026e99e6eb8e06f216c9300e801ba9311349a
Jaseem-v <muhammedjaseem753@gmail.com>
2021-11-04 04:44:35 +0000

    magnificant error fixed

---
+++ assets/js/magnific-popup.js
@@ -0,0 +1,9 @@
+// Magnific Popup
+$(document).ready(function () {
+    $('.portfolio .photography-magnific-popup').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
-
-// Magnific Popup
-$(document).ready(function () {
-    $('.portfolio .photography-magnific-popup').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
-});
-
-// 
 (function (window, document, $, undefined) {
     'use strict';
 
